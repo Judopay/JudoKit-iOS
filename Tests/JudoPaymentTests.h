84215d748a3123ef2acb61bca0921f534ccc2bf0
Hamon Riazy <hamon.riazy@judopayments.com>
2016-02-16 11:55:11 +0000

    adding quick and nimble to project and creating xcworkspace to implement automated tests

---
+++ Tests/JudoPaymentTests.h
@@ -0,0 +1,13 @@
+//
+//  JudoPaymentTests.h
+//  JudoKitObjC
+//
+//  Created by ben.riazy on 16/02/2016.
+//  Copyright © 2016 Judo Payments. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JudoPaymentTests : XCTestCase
+
+@end
