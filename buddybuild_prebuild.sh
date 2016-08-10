17942429179c4df76f40c9ecd663f41d8f13d61f
Ashley Barrett <ashley.barrett@judopayments.com>
2016-08-10 13:24:37 +0100

    added in credentials

---
+++ buddybuild_prebuild.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -f $BUDDYBUILD_SECURE_FILES/JudoTestCaseConfig.swift $BUDDYBUILD_WORKSPACE/Tests/JudoTestCaseConfig.swift
+cp -f $BUDDYBUILD_SECURE_FILES/ExampleAppCredentials.h $BUDDYBUILD_WORKSPACE/JudoKitObjCExampleApp/ExampleAppCredentials.h
