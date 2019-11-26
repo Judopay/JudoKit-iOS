40b3b9caa1d14fac6740a81209bbae3794993830
Mihai Petrenco <mihai.petrenco@endava.com>
2019-11-26 17:34:31 +0200

    Ran clang-format

--- Source/Controller/JudoPayViewController.m
+++ Source/Controller/JudoPayViewController.m
@@ -354,7 +354,7 @@ static inline UIViewAnimationOptions animationOptionsWithCurve(UIViewAnimationCu
         [self.loadingView.rightAnchor constraintEqualToAnchor:self.view.safeRightAnchor],
         [self.loadingView.topAnchor constraintEqualToAnchor:self.view.safeTopAnchor],
         [self.loadingView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
-        
+
         [self.threeDSWebView.leftAnchor constraintEqualToAnchor:self.view.safeLeftAnchor],
         [self.threeDSWebView.rightAnchor constraintEqualToAnchor:self.view.safeRightAnchor],
         [self.threeDSWebView.topAnchor constraintEqualToAnchor:self.view.safeTopAnchor],
@@ -975,19 +975,19 @@ static inline UIViewAnimationOptions animationOptionsWithCurve(UIViewAnimationCu
     [scriptContent appendString:@"meta.name"];
 
     [_threeDSWebView evaluateJavaScript:scriptContent completionHandler:nil];
-    
+
     NSMutableString *removePaResFieldScript = [NSMutableString stringWithString:@"const paResField = document.getElementById('pnPaRESPanel');"];
     [removePaResFieldScript appendString:@"paResField.parentElement.removeChild(paResField);"];
     [removePaResFieldScript appendString:@"paResField.name"];
-    
+
     [_threeDSWebView evaluateJavaScript:removePaResFieldScript completionHandler:nil];
-    
+
     NSMutableString *removePaResButtonScript = [NSMutableString stringWithString:@"const paResButton = document.getElementsByClassName('FormInput')[1];"];
     [removePaResButtonScript appendString:@"paResButton.parentElement.removeChild(paResButton);"];
     [removePaResButtonScript appendString:@"paResButton.name"];
-    
+
     [_threeDSWebView evaluateJavaScript:removePaResButtonScript completionHandler:nil];
-    
+
     CGFloat alphaVal = 1.0f;
     if ([webView.URL.absoluteString isEqualToString:@"about:blank"]) {
         alphaVal = 0.0f;
