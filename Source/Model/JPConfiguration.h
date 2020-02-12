58ee82734b2fc1587633d2fca9e9a709d0d9116f
Mihai Petrenco <mihai.petrenco@endava.com>
2020-02-12 11:48:11 +0200

    Added a configuration file for SDK setup

---
+++ Source/Model/JPConfiguration.h
@@ -0,0 +1,51 @@
+//
+//  JPConfiguration.h
+//  JudoKitObjC
+//
+//  Copyright (c) 2020 Alternative Payments Ltd
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHx3ANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import <Foundation/Foundation.h>
+#import "JPSession.h"
+#import "JPCardNetwork.h"
+
+@class JPAmount, JPReference, JPPaymentMethod, PaymentSummaryItem;
+
+@interface JPConfiguration : NSObject
+
+@property (nonatomic, strong, readonly) NSString *_Nonnull judoId;
+@property (nonatomic, strong, readonly) JPAmount *_Nonnull amount;
+@property (nonatomic, strong, readonly) JPReference *_Nonnull reference;
+@property (nonatomic, strong, readonly) JudoCompletionBlock _Nonnull completion;
+
+- (nonnull instancetype)initWithJudoID:(nonnull NSString *)judoId
+                        amount:(nonnull JPAmount *)amount
+                     reference:(nonnull JPReference *)reference
+                    completion:(nonnull JudoCompletionBlock)completion;
+
+- (void)addPaymentMethods:(NSArray<JPPaymentMethod *> *)methods;
+
+- (void)addSupportedCardNetworks:(CardNetwork)network;
+
+- (void)configureApplePayWithMerchantId:(NSString *)merchantId
+                            countryCode:(NSString *)countryCode
+                    paymentSummaryItems:(NSArray<PaymentSummaryItem *> *)items;
+
+@end
