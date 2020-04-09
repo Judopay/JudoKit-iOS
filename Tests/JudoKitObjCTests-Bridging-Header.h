a95ec67800a9da6c2e9426fab110a45c3e3b9072
Alexei Jovmir <alexei.jovmir@travelport.com>
2020-04-09 11:31:09 +0300

    refactor card validator (remove unused code) constants to card limits unit tests for card validator servise

--- Tests/JudoKitObjCTests-Bridging-Header.h
+++ Tests/JudoKitObjCTests-Bridging-Header.h
@@ -2,7 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "JPPaymentMethodsPresenter.h";
-#import "JPPaymentMethodsViewController.h";
-#import "JPPaymentMethodsInteractor.h";
+#import "JPPaymentMethodsPresenter.h"
+#import "JPPaymentMethodsViewController.h"
+#import "JPPaymentMethodsInteractor.h"
 #import "JPPaymentMethodsViewModel.h"
+#import "JPTransactionInteractor.h"
+#import "JPCardValidationService.h"
+#import "JPCardDetails.h"
