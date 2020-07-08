d596f6290f1297af79c48bf2b6f6af11d0c5d206
Mihai Petrenco <mihai.petrenco@endava.com>
2020-07-08 14:35:54 +0300

    Refactored JPAuthorization and subtypes

--- Source/Models/Authorization/Basic/JPBasicAuthorization.h
+++ Source/Models/Authorization/Basic/JPBasicAuthorization.h
@@ -1,9 +1,52 @@
+//
+//  JPBasicAuthorization.h
+//  JudoKit_iOS
+//
+//  Copyright (c) 2020 Alternative Payments Ltd
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 #import "JPAuthorization.h"
 #import <Foundation/Foundation.h>
 
 @interface JPBasicAuthorization : NSObject <JPAuthorization>
 
-- (instancetype)initWithToken:(NSString *)token andSecret:(NSString *)secret;
-+ (instancetype)authorizationWithToken:(NSString *)token andSecret:(NSString *)secret;
+/**
+ * Designated initializer that describes a basic authorization
+ *
+ * @param token - an NSString instance describing the token
+ * @param secret - an NSString instance describing the secret
+ *
+ * @returns a configured JPBasicAuthorization instance
+ */
+- (instancetype)initWithToken:(NSString *)token
+                    andSecret:(NSString *)secret;
+
+/**
+ * Designated initializer that describes a basic authorization
+ *
+ * @param token - an NSString instance describing the token
+ * @param secret - an NSString instance describing the secret
+ *
+ * @returns a configured JPBasicAuthorization instance
+ */
++ (instancetype)authorizationWithToken:(NSString *)token
+                             andSecret:(NSString *)secret;
 
 @end
--- Source/Models/Authorization/Session/JPSessionAuthorization.m
+++ Source/Models/Authorization/Session/JPSessionAuthorization.m
@@ -1,3 +1,27 @@
+//
+//  JPSessionAuthorization.m
+//  JudoKit_iOS
+//
+//  Copyright (c) 2020 Alternative Payments Ltd
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 #import "JPSessionAuthorization.h"
 #import "Functions.h"
 
@@ -5,22 +29,28 @@
     NSDictionary<NSString *, NSString *> *_headers;
 }
 
-+ (instancetype)authorizationWithToken:(NSString *)token andPaymentSession:(NSString *)session {
-    return [[JPSessionAuthorization alloc] initWithToken:token andPaymentSession:session];
+#pragma mark - Initializers
+
++ (instancetype)authorizationWithToken:(NSString *)token
+                     andPaymentSession:(NSString *)session {
+    
+    return [[JPSessionAuthorization alloc] initWithToken:token
+                                       andPaymentSession:session];
 }
 
-- (instancetype)initWithToken:(NSString *)token andPaymentSession:(NSString *)session {
+- (instancetype)initWithToken:(NSString *)token
+            andPaymentSession:(NSString *)session {
+    
     if (self = [super init]) {
         _headers = @{
             @"Authorization" : generateBasicAuthHeader(token, @""),
             @"Payment-Session" : session
         };
     }
-
     return self;
 }
 
-#pragma mark - Authorization
+#pragma mark - Getters
 
 - (NSDictionary<NSString *, NSString *> *)headers {
     return _headers;
