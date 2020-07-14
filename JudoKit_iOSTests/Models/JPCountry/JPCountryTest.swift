3b9dc8bdbff40c61024989279963cb05d34c1dab
Alexei Jovmir <alexei.jovmir@endava.com>
2020-07-14 16:45:54 +0300

    add test to country update sonar exclusions

---
+++ JudoKit_iOSTests/Models/JPCountry/JPCountryTest.swift
@@ -0,0 +1,61 @@
+//
+//  JPCountryTest.swift
+//  JudoKit_iOSTests
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import XCTest
+@testable import JudoKit_iOS
+
+class JPCountryTest: XCTestCase {
+    
+    /*
+     * GIVEN: String representing UK country
+     *
+     * WHEN: getting iso Code For Country
+     *
+     * THEN: should return 826 numeric code
+     */
+    func test_isoCodeForCountry_WhenUK_ShouldReturnRightISOCode() {
+        XCTAssertEqual(JPCountry.isoCode(forCountry: "UK"), 826)
+    }
+    
+    /*
+     * GIVEN: String representing USA country
+     *
+     * WHEN: getting iso Code For Country
+     *
+     * THEN: should return 840 numeric code
+     */
+    func test_isoCodeForCountry_WhenUSA_ShouldReturnRightISOCode() {
+        XCTAssertEqual(JPCountry.isoCode(forCountry: "USA"), 840)
+    }
+    
+    /*
+     * GIVEN: String representing Canada country
+     *
+     * WHEN: getting iso Code For Country
+     *
+     * THEN: should return 124 numeric code
+     */
+    func test_isoCodeForCountry_WhenCanada_ShouldReturnRightISOCode() {
+        XCTAssertEqual(JPCountry.isoCode(forCountry: "Canada"), 124)
+    }
+    
+}
