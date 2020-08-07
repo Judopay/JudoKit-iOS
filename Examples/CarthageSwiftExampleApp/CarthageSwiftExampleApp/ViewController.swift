c7a0863b92af1b376f72d74401d8a771ab490959
Stefan Popa <stefan.popa@endava.com>
2020-08-07 16:33:21 +0300

    - Added integration example

--- Examples/CarthageSwiftExampleApp/CarthageSwiftExampleApp/ViewController.swift
+++ Examples/CarthageSwiftExampleApp/CarthageSwiftExampleApp/ViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import JudoKit_iOS
 
 class ViewController: UIViewController {
-
+    lazy var judoKit = JudoKit(authorization: JPBasicAuthorization(token: "your-token", andSecret: "your-secret"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func invokeJudoKit(_ sender: Any) {}
+    @IBAction func invokeJudoKit(_ sender: Any) {
+        let amount = JPAmount(amount: "1.5", currency: "GBP")
+        let reference = JPReference.init(consumerReference: "ref-here")
+        let configuration = JPConfiguration(judoID: "121212", amount: amount, reference:reference)
+        judoKit?.invokeTransaction(with: .payment,
+                                   configuration: configuration,
+                                   completion: { (response, error) in
+                                    //todo: handle this callback
+        })
+    }
     
 }
