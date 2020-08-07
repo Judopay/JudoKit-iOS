5335f38820a15012a4544a69bd43c8c19874f443
Stefan Popa <stefan.popa@endava.com>
2020-08-07 16:38:38 +0300

    - Version bump

--- Source/JudoKit.h
+++ Source/JudoKit.h
@@ -32,7 +32,7 @@
 @protocol JPAuthorization;
 
 static NSString *__nonnull const JudoKitName = @"JudoKit_iOS";
-static NSString *__nonnull const JudoKitVersion = @"2.0.0";
+static NSString *__nonnull const JudoKitVersion = @"2.0.1";
 
 @interface JudoKit : NSObject
 
