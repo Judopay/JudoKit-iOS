bc55ded8330c1e81d6d176966bb4acc9d48816f9
Stefan Popa <stefan.popa@endava.com>
2020-08-07 14:11:16 +0300

    - Added Swift sample app using Carthage

---
+++ Examples/CarthageSwiftExampleApp/CarthageSwiftExampleApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CarthageSwiftExampleApp
+//
+//  Copyright © 2020 Alternative Payments Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func invokeJudoKit(_ sender: Any) {}
+    
+}
