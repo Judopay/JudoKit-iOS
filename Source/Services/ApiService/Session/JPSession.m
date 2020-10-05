458a32962fe5b0cab8d1ebf28ddaac47a214198d
Mihai Petrenco <mihai.petrenco@endava.com>
2020-10-05 14:39:53 +0300

    Fixed PR comments

--- Source/Services/ApiService/Session/JPSession.m
+++ Source/Services/ApiService/Session/JPSession.m
@@ -140,8 +140,7 @@ static NSString *const kMethodPUT = @"PUT";
             [request addValue:obj forHTTPHeaderField:key];
         }];
 
-        //TODO: Temporary fix for bank/sale requests
-        if ([path isEqualToString:@"order/bank/sale"] || [path containsString:@"order/bank/statusrequest"]) {
+        if ([path hasPrefix:@"order/bank"]) {
             [request setValue:@"2.0.0" forHTTPHeaderField:@"API-Version"];
         }
 
