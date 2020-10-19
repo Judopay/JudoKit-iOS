a7e76025d14177eae4153d93671c4547fe87d26e
rk <rk@RKs-MacBook-Pro.local>
2020-10-19 10:48:47 +0530

    When ever the user manually dismisses apple pay, the event should be passed to the caller via callback

--- Source/Services/ApplePay/JPApplePayController.m
+++ Source/Services/ApplePay/JPApplePayController.m
@@ -67,6 +67,7 @@
 
 - (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
     [controller dismissViewControllerAnimated:YES completion:nil];
+    self.completionBlock(nil, [NSError judoUserDidCancelError]);
 }
 
 @end
