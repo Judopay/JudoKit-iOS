1de5254b891bb0d5b7404dc8e5450b328ffa10c4
Stefan Popa <stefan.popa@endava.com>
2021-01-20 00:30:06 +0200

    - Added unit tests

--- JudoKit_iOSTests/Models/JPCardDetails/JPCardDetailsTest.swift
+++ JudoKit_iOSTests/Models/JPCardDetails/JPCardDetailsTest.swift
@@ -149,4 +149,182 @@ class JPCardDetailsTest: XCTestCase {
         cardDetails.cardNetwork = JPCardNetworkType(rawValue: 0)
         XCTAssertEqual(cardDetails.formattedCardLastFour(), "**** 1111")
     }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is Visa
+     *
+     * THEN: should parse cardNetwork as Visa
+     */
+    func test_ParsingCardType_WhenVisa_ShouldReturnVisaNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 1
+        let cardDetails1 = JPCardDetails(dictionary: dic)
+        
+        dic["cardType"] = 3
+        let cardDetails2 = JPCardDetails(dictionary: dic)
+
+        dic["cardType"] = 11
+        let cardDetails3 = JPCardDetails(dictionary: dic)
+
+        dic["cardType"] = 13
+        let cardDetails4 = JPCardDetails(dictionary: dic)
+
+        XCTAssertEqual(cardDetails1.cardNetwork, JPCardNetworkType.visa)
+        XCTAssertEqual(cardDetails2.cardNetwork, JPCardNetworkType.visa)
+        XCTAssertEqual(cardDetails3.cardNetwork, JPCardNetworkType.visa)
+        XCTAssertEqual(cardDetails4.cardNetwork, JPCardNetworkType.visa)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is MasterCard
+     *
+     * THEN: should parse cardNetwork as MasterCard
+     */
+    func test_ParsingCardType_WhenMasterCard_ShouldReturnMasterCardNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 2
+        let cardDetails1 = JPCardDetails(dictionary: dic)
+        
+        dic["cardType"] = 12
+        let cardDetails2 = JPCardDetails(dictionary: dic)
+
+        XCTAssertEqual(cardDetails1.cardNetwork, JPCardNetworkType.masterCard)
+        XCTAssertEqual(cardDetails2.cardNetwork, JPCardNetworkType.masterCard)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is ChinaUnionPay
+     *
+     * THEN: should parse cardNetwork as ChinaUnionPay
+     */
+    func test_ParsingCardType_WhenChinaUnionPay_ShouldReturnChinaUnionPayNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 7
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.chinaUnionPay)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is AMEX
+     *
+     * THEN: should parse cardNetwork as AMEX
+     */
+    func test_ParsingCardType_WhenAMEX_ShouldReturnAMEXNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 8
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.AMEX)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is JCB
+     *
+     * THEN: should parse cardNetwork as JCB
+     */
+    func test_ParsingCardType_WhenJCB_ShouldReturnJCBNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 9
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.JCB)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is Maestro
+     *
+     * THEN: should parse cardNetwork as Maestro
+     */
+    func test_ParsingCardType_WhenMaestro_ShouldReturnMaestroNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 10
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.maestro)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is Discover
+     *
+     * THEN: should parse cardNetwork as Discover
+     */
+    func test_ParsingCardType_WhenDiscover_ShouldReturnDiscoverNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 14
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.discover)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is DinersClub
+     *
+     * THEN: should parse cardNetwork as DinersClub
+     */
+    func test_ParsingCardType_WhenDinersClub_ShouldReturnDinersClubNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 17
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.dinersClub)
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is Unknown
+     *
+     * THEN: should parse cardNetwork as Unknown
+     */
+    func test_ParsingCardType_WhenUnknown_ShouldReturnUnknownNetwork() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = -1
+        let cardDetails = JPCardDetails(dictionary: dic)
+        
+        XCTAssertEqual(cardDetails.cardNetwork, [])
+    }
+    
+    /*
+     * GIVEN: Creating JPCardDetails from dictionary
+     *
+     * WHEN: card network is Unknown
+     * AND: cardNumber is known
+     *
+     * THEN: should return cardNetwork based on given cardNumber
+     */
+    func test_ParsingCardType_WhenUnknown_And_CardNumberIsKnown_ShouldReturnNetworkBasedOnCardNumber() {
+        var dic = cardDetailsDic["cardDetails"]! as! [AnyHashable : Any]
+        
+        dic["cardType"] = 100
+        let cardDetails = JPCardDetails(dictionary: dic)
+        cardDetails.cardNumber = "4111 1111 1111 1111"
+        
+        XCTAssertEqual(cardDetails.cardNetwork, JPCardNetworkType.visa)
+    }
 }
