5c371e68b28d755fe4973784c2ad32f70295191a
stpopa <51994215+stpopa@users.noreply.github.com>
2021-02-01 10:50:22 +0200

    - Added missing currencies (#112)

--- Source/Services/ConfigurationValidation/JPConfigurationValidationService.m
+++ Source/Services/ConfigurationValidation/JPConfigurationValidationService.m
@@ -156,14 +156,21 @@
 }
 
 - (NSArray *)validCurrencies {
-    return @[ @"AED", @"AUD", @"BRL",
-              @"CAD", @"CHF", @"CZK",
-              @"DKK", @"EUR", @"GBP",
-              @"HKD", @"HUF", @"JPY",
-              @"NOK", @"NZD", @"PKR",
-              @"PLN", @"SEK", @"SGD",
-              @"QAR", @"SAR", @"USD",
-              @"ZAR" ];
+    return @[ @"GBP", @"USD", @"EUR",
+              @"AUD", @"SEK", @"CAD",
+              @"NOK", @"BRL", @"CHF",
+              @"CZK", @"DKK", @"HKD",
+              @"HUF", @"JPY", @"NZD",
+              @"PLN", @"ZAR", @"ARS",
+              @"BHD", @"MMK", @"KYD",
+              @"CLP", @"CNY", @"COP",
+              @"ISK", @"INR", @"IDR",
+              @"JOD", @"KWD", @"OMR",
+              @"NGN", @"PKR", @"NIO",
+              @"PAB", @"PHP", @"QAR",
+              @"RUB", @"SAR", @"SGD",
+              @"VND", @"AED", @"RSD",
+              @"RON", @"MXN", @"UAH"];
 }
 
 @end
