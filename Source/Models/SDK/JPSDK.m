6c8b4c90afcf8817ec4bb4f0a593c4dc1d5f8e23
Andrei Movila <andrei.movila@endava.com>
2021-04-08 03:31:20 +0300

    add JPThreeDSecureTwo

---
+++ Source/Models/SDK/JPSDK.m
@@ -0,0 +1,45 @@
+//
+//  JPSDK.m
+//  JudoKit_iOS
+//
+//  Copyright (c) 2021 Alternative Payments Ltd
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import "JPSDK.h"
+#import "JPEphemeralPublicKey.h"
+#import "JPDeviceRenderOptions.h"
+
+@implementation JPSDK
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    if (self = [super init]) {
+       _applicationId = dictionary[@"applicationID"];
+       
+        _encodedData = dictionary[@"encodedData"];
+        _maxTimeout = dictionary[@"maxTimeout"];
+        _referenceNumber = dictionary[@"referenceNumber"];
+        _transactionId = dictionary[@"transactionId"];
+        _ephemeralPublicKey = [[JPEphemeralPublicKey alloc] initWithDictionary:dictionary[@"ephemeralPublicKey"]];
+        _ephemeralPublicKey = [[JPDeviceRenderOptions alloc] initWithDictionary:dictionary[@"deviceRenderOptions"]];
+    }
+    return self;
+}
+
+@end
