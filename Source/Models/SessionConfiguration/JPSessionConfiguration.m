70bdee5d9c62d0ba98b9461c333d9cca1837a99f
Andrei Movila <andrei.movila@endava.com>
2021-04-12 11:11:56 +0300

    use prod static API Version and BaseURL

--- Source/Models/SessionConfiguration/JPSessionConfiguration.m
+++ Source/Models/SessionConfiguration/JPSessionConfiguration.m
@@ -26,7 +26,7 @@
 #import "JPAuthorization.h"
 
 static NSString *const kJudoBaseURL = @"https://api.judopay.com/";
-static NSString *const kJudoSandboxBaseURL = @"https://gw1.karatepay-sandbox.com/";
+static NSString *const kJudoSandboxBaseURL = @"https://api-sandbox.judopay.com/";
 
 @implementation JPSessionConfiguration
 
