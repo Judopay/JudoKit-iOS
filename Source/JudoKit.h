d35f2ba3921468ae83dd054dfc695f197f768ac8
Stefan <stefan.popa@judopayments.com>
2022-07-22 15:00:45 +0300

    Fixes configuration model interface attributes (#149)
    * Fixes configuration model interface attributes
    
    * Fixes pbxproj file
    
    * SMP setup

--- Source/JudoKit.h
+++ Source/JudoKit.h
@@ -32,7 +32,7 @@
 @protocol JPAuthorization;
 
 static NSString *__nonnull const JudoKitName = @"JudoKit_iOS";
-static NSString *__nonnull const JudoKitVersion = @"3.0.1";
+static NSString *__nonnull const JudoKitVersion = @"3.0.2";
 
 @interface JudoKit : NSObject
 
