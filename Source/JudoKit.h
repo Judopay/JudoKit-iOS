13c333c0bae199b8f7144b736692c7fc3d736136
Stefan Popa <stefan.popa@judopayments.com>
2022-08-05 18:51:04 +0300

    Package version bump

--- Source/JudoKit.h
+++ Source/JudoKit.h
@@ -32,7 +32,7 @@
 @protocol JPAuthorization;
 
 static NSString *__nonnull const JudoKitName = @"JudoKit_iOS";
-static NSString *__nonnull const JudoKitVersion = @"3.0.3";
+static NSString *__nonnull const JudoKitVersion = @"3.1.0";
 
 @interface JudoKit : NSObject
 
