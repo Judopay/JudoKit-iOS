3bf343bb77d88312cf3c4c8fcedda1f6abb214e0
Stefan <stefan.popa@judopayments.com>
2023-01-24 18:54:13 +0200

    Fixes SPM broken symlink `Source/include/JPPreAuthPaymentRequest.h` (#175)

--- Source/include/JPPreAuthPaymentRequest.h
+++
@@ -1,1 +0,0 @@
-../Models/Request/JPPreAuthPaymentRequest.h
--- Source/include/create_symlinks.sh
+++ Source/include/create_symlinks.sh
@@ -76,7 +76,7 @@ ln -s -f ../Services/ApiService/JPApiService.h JPApiService.h
 ln -s -f ../Services/ApiService/Session/JPSession.h JPSession.h
 ln -s -f ../Models/Request/JPRequest.h JPRequest.h
 ln -s -f ../Models/Request/JPBankOrderSaleRequest.h JPBankOrderSaleRequest.h
-ln -s -f ../Models/Request/JPPreAuthPaymentRequest.h JPPreAuthPaymentRequest.h
+ln -s -f ../Models/Request/JPPreAuthRequest.h JPPreAuthRequest.h
 ln -s -f ../Models/Request/JPPreAuthTokenRequest.h JPPreAuthTokenRequest.h
 ln -s -f ../Models/Request/JPPreAuthApplePayRequest.h JPPreAuthApplePayRequest.h
 ln -s -f ../Models/Request/JPPaymentRequest.h JPPaymentRequest.h
