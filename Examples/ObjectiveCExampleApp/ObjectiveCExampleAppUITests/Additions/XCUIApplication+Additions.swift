dacd547674c9aba5704c4acb4157c75c67a36634
Eugene Zhernakov <eugene.zhernakov@gmail.com>
2023-07-10 17:33:09 +0100

    Use environment variables for credentials

--- Examples/ObjectiveCExampleApp/ObjectiveCExampleAppUITests/Additions/XCUIApplication+Additions.swift
+++ Examples/ObjectiveCExampleApp/ObjectiveCExampleAppUITests/Additions/XCUIApplication+Additions.swift
@@ -104,9 +104,13 @@ extension XCUIApplication {
     }
     
     func configureSettings() {
-        launchArguments += ["-judo_id", "judo_id",
-                            "-token", "token",
-                            "-secret", "secret",
+        let judoID = ProcessInfo.processInfo.environment["TEST_API_JUDO_ID"]
+        let apiToken = ProcessInfo.processInfo.environment["TEST_API_TOKEN"]
+        let apiSecret = ProcessInfo.processInfo.environment["TEST_API_SECRET"]
+        
+        launchArguments += ["-judo_id", judoID ?? "",
+                            "-token", apiToken ?? "",
+                            "-secret", apiSecret ?? "",
                             "-is_sandboxed", "true",
                             "-is_token_and_secret_on", "true",
                             "-should_ask_for_billing_information", "false"]
