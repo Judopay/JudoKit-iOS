761c958ab3ef8445928e102b810e07e885db5f0a
Stefan Popa <popa.stefan.tudor@gmail.com>
2023-10-26 13:17:48 +0300

    Attempt 2 to fix Carthage timeouts

--- scripts/carthage_example_carthage_bootstrap.sh
+++ scripts/carthage_example_carthage_bootstrap.sh
@@ -25,18 +25,15 @@ git_hash="$(git rev-parse HEAD)"
 
 echo "git \"${git_repo}\" \"${git_hash}\"" > "${carthage_example_app_dir}/Cartfile"
 
+info "Resolving SPM packages as a workaround for Cartahge timeouts..."
+xcodebuild -resolvePackageDependencies -project "${judo_kit_dir}/Examples/SPMExampleApp/SPMExampleApp.xcodeproj" -scheme SPMExampleApp
+
 # Execute carthage bootstrap
 info "Executing carthage bootstrap..."
 
 cd "${carthage_example_app_dir}" || error "Executing \`cd\` failed"
 
-# temporary workaround for `xcodebuild timed out while trying to read SPMExampleApp.xcodeproj`
-carthage bootstrap --platform ios --no-build
-
-info "Removing example apps files..."
-rm -rf "${carthage_example_app_dir}/Carthage/Checkouts/JudoKit-iOS/Examples"
-
-carthage build --platform ios --use-xcframeworks --cache-builds
+carthage bootstrap --platform ios --use-xcframeworks --cache-builds --verbose 
 
 carthage_exit_code="$?"
 
