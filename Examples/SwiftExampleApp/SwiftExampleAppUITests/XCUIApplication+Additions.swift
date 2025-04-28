e1aefafe8ec66309135724086d850530835d62e2
Eugene Zhernakov <eugene.zhernakov@gmail.com>
2025-04-28 11:43:23 +0100

    Add auth details back in

--- Examples/SwiftExampleApp/SwiftExampleAppUITests/XCUIApplication+Additions.swift
+++ Examples/SwiftExampleApp/SwiftExampleAppUITests/XCUIApplication+Additions.swift
@@ -135,6 +135,10 @@ extension XCUIApplication {
         let apiToken = ProcessInfo.processInfo.environment["TEST_API_TOKEN"]
         let apiSecret = ProcessInfo.processInfo.environment["TEST_API_SECRET"]
 
+        launchArguments += ["-judo_id", judoID ?? "",
+                            "-token", apiToken ?? "",
+                            "-secret", apiSecret ?? ""]
+
         launchArguments += ["-is_sandboxed", "true",
                             "-is_token_and_secret_on", "true",
                             "-should_ask_for_billing_information", "false",
