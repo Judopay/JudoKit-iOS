fd48fbf5d29cf3d84436996ac74ab997fa205789
Stefan <stefan.popa@judopayments.com>
2025-07-30 15:52:50 +0300

    Package version bump (#308)

--- Source/Models/Constants/JPConstants.h
+++ Source/Models/Constants/JPConstants.h
@@ -28,7 +28,7 @@
 #import <Foundation/Foundation.h>
 
 static NSString *const kJudoKitName = @"JudoKit_iOS";
-static NSString *const kJudoKitVersion = @"5.0.1";
+static NSString *const kJudoKitVersion = @"5.0.2";
 
 /* Patterns */
 static NSString *const kDefaultPattern = @"XXXX XXXX XXXX XXXX";
